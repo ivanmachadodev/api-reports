8cdae0d1e45b89235ad3575858109fe7891d886e
sebakatzmanoversoft <sebastian.katzman@oversoft.net>
2024-01-22 16:56:16 -0300

    Add Test

---
+++ API.ReportsEngine/XUnitTest/DetFiesIdOfDataSetTest.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using API.Application.Commands.AreaCommands;
+using API.Application.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using API.ReportsEngine.Controllers;
+
+namespace XUnitTest
+{
+    public class DetFiesIdOfDataSetTest
+    {
+       
+
+    }
+}
